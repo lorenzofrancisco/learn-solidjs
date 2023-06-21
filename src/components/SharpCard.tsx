2e25b80cf29a15e3069bce50fbf9a7bcd1214976
lozo6 <99729303+lozo6@users.noreply.github.com>
2023-06-21 11:17:06 -0400

    interactive like btn

--- src/components/SharpCard.tsx
+++ src/components/SharpCard.tsx
@@ -1,26 +1,56 @@
+import { likedSharps, setLikedSharps } from "../pages/Favorites";
+
+const likeSharp = (sharp) => {
+  setLikedSharps([sharp, ...likedSharps()]);
+};
+
+const unLikeSharp = (sharpId) => {
+  const sharpState = likedSharps()?.filter(
+    (item) => item.project_title !== sharpId
+  );
+  setLikedSharps(sharpState);
+};
+
+const isFavorite = (sharpId: string) => {
+  const sharps = likedSharps()?.filter(
+    (item) => item.project_title === sharpId
+  );
+  return sharps?.length > 0;
+};
+
 const SharpCard = ({ sharp }) => {
-    return (
-        <div class="card mb-3">
-          <div class="card-header bg-primary text-white">
-            Type is {sharp.project_output_type}
-          </div>
-          <div class="card-body">
-            <p>
-              <strong>Title: </strong> {sharp.project_title}
-            </p>
-            <p>
-              <strong>Contributors: </strong> {sharp.contributors_team}
-            </p>
-            <p class="card-text">
-              <strong>Description: </strong> {sharp.project_description}
-            </p>
-          </div>
-          <div class="card-footer">
-            <button class="btn btn-primary">Like</button>
-          </div>
-        </div>
-        
-    );
+  return (
+    <div class="card mb-3">
+      <div class="card-header bg-primary text-white">
+        Type is {sharp.project_output_type}
+      </div>
+      <div class="card-body">
+        <p>
+          <strong>Title: </strong> {sharp.project_title}
+        </p>
+        <p>
+          <strong>Contributors: </strong> {sharp.contributors_team}
+        </p>
+        <p class="card-text">
+          <strong>Description: </strong> {sharp.project_description}
+        </p>
+      </div>
+      <div class="card-footer">
+        {isFavorite(sharp.project_title) ? (
+          <button
+            class="btn btn-danger"
+            onclick={() => unLikeSharp(sharp.project_title)}
+          >
+            UnLike
+          </button>
+        ) : (
+          <button class="btn btn-primary" onclick={() => likeSharp(sharp)}>
+            Like
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SharpCard;
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,17 @@
+import { createEffect, createSignal, For } from "solid-js";
+import SharpCard from "../components/SharpCard";
+
+const [likedSharps , setLikedSharps] = createSignal([]);
+const Favorites = () => {
+  return (
+    <div>
+        <h1>Favorites</h1>
+        <div>
+          <For each={likedSharps()}>{(sharp) => <SharpCard sharp={sharp} />}</For>
+        </div>
+    </div>
+    );
+};
+
+export { likedSharps, setLikedSharps };
+export default Favorites;
